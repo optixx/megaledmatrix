ee814285566e3c65c0951b7585d214fd42616982
david <david@optixx.org>
2009-01-10 22:43:30 +0100

    o cleanup

--- pwm/.dep/ledm.o.d
+++
@@ -1,9 +0,0 @@
-ledm.o: ledm.c ledm.h \
-  /opt/local/lib/gcc/avr/4.0.2/../../../../avr/include/inttypes.h \
-  /opt/local/lib/gcc/avr/4.0.2/../../../../avr/include/stdint.h
-
-ledm.h:
-
-/opt/local/lib/gcc/avr/4.0.2/../../../../avr/include/inttypes.h:
-
-/opt/local/lib/gcc/avr/4.0.2/../../../../avr/include/stdint.h:
